46b532e3e06800f1ceb69af969eb7f91ce56a9f8
Thomas Ongeri <ongerit@gmail.com>
2017-07-20 12:10:12 -0400

    modified hover

--- pages/industries.js
+++ pages/industries.js
@@ -3,9 +3,11 @@ import {ContentSection, Body} from '../components/content-section/content-sectio
 import Industries from  '../static/data/products/industries/industries.json';
 import IndustriesHero from './industries/hero';
 import IndustriesMain from './industries/main';
+import styles from './industries/industries.styles.scss'
 
 export default () => (
     <Layout meta={Industries.meta}>
+      <style dangerouslySetInnerHTML={{__html: styles}}/>
         <IndustriesHero header={Industries.header} />
         <ContentSection className="background-color-light-1 center">
           <Body>
