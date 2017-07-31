9b9128a4c3e52d81f4c4caa62c3d6095606b7c79
Thomas Ongeri <ongerit@gmail.com>
2017-07-31 14:43:59 -0400

    added reversal of devices

--- pages/single-industries/main.js
+++ pages/single-industries/main.js
@@ -10,15 +10,6 @@ class IndustriesMain extends React.Component {
   }
 
   render() {
-
-    const orderZero = {
-      order: 0
-    };
-
-    const orderOne = {
-      order: 1
-    };
-
     const sponsorImage = {
       width: '100%'
     }
@@ -52,13 +43,13 @@ class IndustriesMain extends React.Component {
     const sectionItems = (data.sections) ?
     data.sections.map( (section, key)=> {
       return(
-        <ContentSection key={key} style={{'position' : 'relative'}}>
+        <ContentSection className="single-industries" key={key} style={{'position' : 'relative'}}>
               <ContentPane half graphic>
-                    { 
+                    {
                       section.content.map((item, index) => <DeviceWrapper key={index} content={item} alt={'Section Image'}></DeviceWrapper>)
                     }
               </ContentPane>
-              <ContentPane half center-vertically text style={ (key % 2) ? orderOne : orderZero}>
+              <ContentPane half center-vertically text >
                   <Title> {section.title}</Title>
                   <p dangerouslySetInnerHTML={{__html: section.body}}/>
               </ContentPane>
