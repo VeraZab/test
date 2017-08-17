06d20bd39fd1dc22f62228eb9ab5074a22ce2277
Thomas Osmonson <thomas.osmonson@gmail.com>
2017-08-17 11:13:36 -0500

    try to fix exports

--- out/_next/62026c5c-fce9-4ef1-b3c1-4fa01aa2d49b/page/d3-js-for-python-and-pandas-charts/code-explorer/code-samples/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/d3-js-for-python-and-pandas-charts/code-explorer/code-samples', function() {
-            var comp = module.exports=webpackJsonp([57],{655:function(o,t,e){o.exports=e(656)},656:function(o,t,e){"use strict"}},[655]);
-            return { page: comp.default }
-          })
-        
--- out/_next/62026c5c-fce9-4ef1-b3c1-4fa01aa2d49b/page/plotly-js-scientific-d3-charting-library/code-explorer/code-samples/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/plotly-js-scientific-d3-charting-library/code-explorer/code-samples', function() {
-            var comp = module.exports=webpackJsonp([47],{735:function(o,t,e){o.exports=e(736)},736:function(o,t,e){"use strict"}},[735]);
-            return { page: comp.default }
-          })
-        
