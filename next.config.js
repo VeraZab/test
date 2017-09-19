f08a9d15133a2cefd1adc42545eca0c0945491eb
Thomas Osmonson <thomas.osmonson@gmail.com>
2017-09-19 20:16:05 +0200

    assetPrefix

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const chromatic = require("chromatic-sass")
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-    assetPrefix: isProd ? 'https://plotly.github.io/plotly-next/' : '', // for building and static export
+    assetPrefix: 'https://plotly.github.io/plotly-next/',
     exportPathMap: () => ({
         "/": { page: "/" },
         "/dashboards": { page: "/dashboards" },
