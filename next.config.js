d1a6908781992dcfdc2c825287bd94e39726207e
Thomas Osmonson <thomas.osmonson@gmail.com>
2017-12-19 10:43:15 -0600

    add static export for dash pricing page

--- next.config.js
+++ next.config.js
@@ -1,177 +1,184 @@
 const path = require('path');
 const glob = require('glob');
-const isProd = process.env.NODE_ENV === 'production'
-const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
+const isProd = process.env.NODE_ENV === 'production';
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 
 module.exports = {
   exportPathMap: () => ({
     '/': {
       page: '/p',
       query: {
-        slug: 'home'
-      }
+        slug: 'home',
+      },
     },
     // Dashboards
     '/dashboards': {
       page: '/p',
       query: {
-        slug: 'dashboards-and-reporting'
-      }
+        slug: 'dashboards-and-reporting',
+      },
     },
     '/dashboards-and-reports': {
       page: '/p',
       query: {
-        slug: 'dashboards-and-reporting'
-      }
+        slug: 'dashboards-and-reporting',
+      },
     },
     // Falcon
     '/database-connectors': {
       page: '/p',
       query: {
-        slug: 'falcon-sql-editor'
-      }
+        slug: 'falcon-sql-editor',
+      },
     },
     '/free-sql-client-download': {
       page: '/p',
       query: {
-        slug: 'falcon-sql-editor'
-      }
+        slug: 'falcon-sql-editor',
+      },
     },
     // Chart Studio
     '/online-chart-maker': {
       page: '/p',
       query: {
-        slug: 'chart-studio'
-      }
+        slug: 'chart-studio',
+      },
     },
     // Slide Decks
     '/powerpoint-online': {
       page: '/p',
       query: {
-        slug: 'slide-decks'
-      }
+        slug: 'slide-decks',
+      },
     },
     '/online-presentation-tool': {
       page: '/p',
       query: {
-        slug: 'slide-decks'
-      }
+        slug: 'slide-decks',
+      },
     },
     // Plotly.js
     '/plotly-js-scientific-d3-charting-library': {
       page: '/p',
       query: {
-        slug: 'plotly-js'
-      }
+        slug: 'plotly-js',
+      },
     },
     // Plotly.R
     '/d3-js-for-r-and-shiny-charts': {
       page: '/p',
       query: {
-        slug: 'plotly-r'
-      }
+        slug: 'plotly-r',
+      },
     },
     // Plotly.py
     '/d3-js-for-python-and-pandas-charts': {
       page: '/p',
       query: {
-        slug: 'plotly-py'
-      }
+        slug: 'plotly-py',
+      },
     },
     // Dash
-    '/products/dash': {page: '/products/dash'},
+    '/products/dash': { page: '/products/dash' },
+    // Dash Pricing
+    '/dash/pricing': {
+      page: '/p',
+      query: {
+        slug: 'dash-services',
+      },
+    },
     // Pricing - cloud
-    '/products/cloud': {page: '/products/cloud'},
-    '/products/cloud/compare': {page: '/products/cloud'},
+    '/products/cloud': { page: '/products/cloud' },
+    '/products/cloud/compare': { page: '/products/cloud' },
     // Pricing - on premises
-    '/products/on-premise': {page: '/products/on-premise'},
-    '/products/on-premises': {page: '/products/on-premise'},
+    '/products/on-premise': { page: '/products/on-premise' },
+    '/products/on-premises': { page: '/products/on-premise' },
     // Consulting and OEM (Advanced Development)
-    '/products/consulting-and-oem': {page: '/products/consulting-and-oem'},
+    '/products/consulting-and-oem': { page: '/products/consulting-and-oem' },
 
-    '/webinars': {page: '/webinars'},
-    '/export': {page: '/export'},
+    '/webinars': { page: '/webinars' },
+    '/export': { page: '/export' },
 
     // Industries
-    '/products/industries': {page: '/industries'},
+    '/products/industries': { page: '/industries' },
 
     '/products/industries/finance': {
       page: '/single-industries',
-      query: {id: 'finance'}
+      query: { id: 'finance' },
     },
     '/products/industries/finance-analytics': {
       page: '/single-industries',
-      query: {id: 'finance'}
+      query: { id: 'finance' },
     },
     '/products/industries/aerospace': {
       page: '/single-industries',
-      query: {id: 'aerospace'}
+      query: { id: 'aerospace' },
     },
     '/products/industries/aerospace-analytics': {
       page: '/single-industries',
-      query: {id: 'aerospace'}
+      query: { id: 'aerospace' },
     },
     '/products/industries/automotive-analytics': {
       page: '/single-industries',
-      query: {id: 'automotive-analytics'}
+      query: { id: 'automotive-analytics' },
     },
     '/products/industries/business-intelligence': {
       page: '/single-industries',
-      query: {id: 'business-intelligence'}
+      query: { id: 'business-intelligence' },
     },
     '/products/industries/business-intelligence-analytics': {
       page: '/single-industries',
-      query: {id: 'business-intelligence'}
+      query: { id: 'business-intelligence' },
     },
     '/products/industries/energy-analytics': {
       page: '/single-industries',
-      query: {id: 'energy-analytics'}
+      query: { id: 'energy-analytics' },
     },
     '/products/industries/government-and-public-analytics': {
       page: '/single-industries',
-      query: {id: 'government-and-public-analytics'}
+      query: { id: 'government-and-public-analytics' },
     },
     '/products/industries/insurance': {
       page: '/single-industries',
-      query: {id: 'insurance'}
+      query: { id: 'insurance' },
     },
     '/products/industries/insurance-analytics': {
       page: '/single-industries',
-      query: {id: 'insurance'}
+      query: { id: 'insurance' },
     },
     '/products/industries/journalism': {
       page: '/single-industries',
-      query: {id: 'journalism'}
+      query: { id: 'journalism' },
     },
     '/products/industries/pharma-analytics': {
       page: '/single-industries',
-      query: {id: 'pharma-analytics'}
+      query: { id: 'pharma-analytics' },
     },
     '/products/industries/research-and-development-analytics': {
       page: '/single-industries',
-      query: {id: 'research-and-development-analytics'}
+      query: { id: 'research-and-development-analytics' },
     },
     // Customer Stories
-    '/products/enterprise/customer-stories': {page: '/customer-stories'},
-    '/products/on-premise/customer-stories': {page: '/customer-stories'},
+    '/products/enterprise/customer-stories': { page: '/customer-stories' },
+    '/products/on-premise/customer-stories': { page: '/customer-stories' },
     '/products/enterprise/customer-stories/c12-energy': {
       page: '/p-customer-story',
-      query: {slug: 'c12-energy'}
+      query: { slug: 'c12-energy' },
     },
     '/products/on-premise/customer-stories/c12-energy': {
       page: '/p-customer-story',
-      query: {slug: 'c12-energy'}
+      query: { slug: 'c12-energy' },
     },
     '/products/enterprise/customer-stories/goji': {
       page: '/p-customer-story',
-      query: {slug: 'goji'}
+      query: { slug: 'goji' },
     },
     '/products/on-premise/customer-stories/goji': {
       page: '/p-customer-story',
-      query: {slug: 'goji'}
-    }
+      query: { slug: 'goji' },
+    },
   }),
-  webpack: (config, {dev}) => {
+  webpack: (config, { dev }) => {
     config.plugins.push(
       new SWPrecacheWebpackPlugin({
         minify: true,
@@ -180,9 +187,9 @@ module.exports = {
         runtimeCaching: [
           {
             handler: 'networkFirst',
-            urlPattern: /^https?.*/
-          }
-        ]
+            urlPattern: /^https?.*/,
+          },
+        ],
       })
     );
     config.module.rules.push(
@@ -190,12 +197,12 @@ module.exports = {
         test: /\.(css|scss)/,
         loader: 'emit-file-loader',
         options: {
-          name: 'dist/[path][name].[ext]'
-        }
+          name: 'dist/[path][name].[ext]',
+        },
       },
       {
         test: /\.css$/,
-        use: ['babel-loader', 'raw-loader', 'postcss-loader']
+        use: ['babel-loader', 'raw-loader', 'postcss-loader'],
       },
       {
         test: /\.s(a|c)ss$/,
@@ -210,12 +217,12 @@ module.exports = {
               includePaths: ['./', 'styles', 'node_modules']
                 .map(d => path.join(__dirname, d))
                 .map(g => glob.sync(g))
-                .reduce((a, c) => a.concat(c), [])
-            }
-          }
-        ]
+                .reduce((a, c) => a.concat(c), []),
+            },
+          },
+        ],
       }
     );
     return config;
-  }
+  },
 };
