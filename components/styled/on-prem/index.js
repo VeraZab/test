46aaf047dad41c639d2f72926a2b521b6b6a55d9
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-01-31 17:03:47 -0600

    cleanup

--- components/styled/on-prem/index.js
+++ components/styled/on-prem/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 const SwitchContainer = styled.div`
   display: flex;
@@ -19,87 +19,6 @@ const HeadingContainer = styled.div`
   flex-direction: column;
   padding-top: 20px;
   padding-bottom: 40px;
-`;
-
-const Switch = styled.div`
-  display: flex;
-  overflow: hidden;
-  border-radius: 48px;
-  width: 100%;
-  max-width: 360px;
-  height: 48px;
-  background: white;
-  border: 1px solid #dce4ed;
-  box-shadow: rgba(80, 103, 132, 0.2) 0 2px 8px 0;
-`;
-
-const hoverSyles = css`
-  &:hover {
-    cursor: pointer;
-    color: rgba(81, 103, 131, 1);
-    font-weight: 400;
-
-    span {
-      &::before {
-        transform: none;
-      }
-    }
-  }
-`;
-
-const SwitchItem = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-grow: 1;
-  width: 50%;
-  cursor: default;
-  background-color: ${props => (props.active ? 'transparent' : 'transparent')};
-  color: ${props => (props.active ? '#ffffff' : 'rgba(81, 103, 131, 0.75)')};
-  font-weight: ${props => (props.active ? 'bold' : 'normal')};
-  text-shadow: ${props =>
-    props.active ? '0 1px 2px rgba(42, 63, 95, 0.5)' : 'none'};
-  ${props => (props.active ? null : hoverSyles)};
-
-  user-select: none;
-  transition: 0.1s all ease-in-out;
-  position: relative;
-
-  &::before {
-    position: absolute;
-    content: '';
-    width: 120%;
-    height: 100%;
-    opacity: ${props => (props.active ? '1' : '0')};
-    background-color: #00cc96;
-    top: 0;
-    left: ${props => (props.index.toString() === "1" ? 'auto' : '-20%')};
-    right: ${props => (props.index.toString() === "1" ? '-20%' : 'auto')};
-    transform-origin: center center;
-    will-change: transform;
-    transition: 0.2s transform ease-in-out;
-    transform: ${props =>
-      props.active
-        ? props.index.toString() === "1" ? 'skewX(-30deg)' : 'skewX(30deg)'
-        : props.index.toString() === "1" ? 'skewX(30deg)' : 'skewX(-30deg)'};
-  }
-
-  span {
-    display: inline-block;
-    position: relative;
-    &::before {
-      position: absolute;
-      content: '';
-      width: 100%;
-      height: 2px;
-      border-radius: 4px;
-      background-color: #00cc96;
-      top: 100%;
-      transform-origin: center center;
-      transform: scaleX(0);
-      transition: 0.1s all ease-in-out;
-    }
-  }
 `;
 
 const H1 = styled.h1`
@@ -129,4 +48,4 @@ const H2 = styled.h2`
   will-change: opacity, transform;
 `;
 
-export { SwitchContainer, HeadingContainer, Switch, SwitchItem, H1, H2 };
+export { SwitchContainer, HeadingContainer, H1, H2 };
