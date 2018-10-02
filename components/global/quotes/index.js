5762314386c61b731d28f8d340d39c663f07137e
Joseph Damiba <joseph.damiba@plot.ly>
2018-10-02 11:53:52 -0400

    refactored styles into .scss external stylesheet

--- components/global/quotes/index.js
+++ components/global/quotes/index.js
@@ -1,21 +1,12 @@
 import React from 'react'
-import { LogicGateOrIcon } from 'mdi-react';
+import styles from './quotes.scss'
 
 class Quotes extends React.Component {
   constructor(props) {
     super(props)
   }
   render() {
-    const titleStyle = {paddingTop:"30px", paddingBottom:"30px", textAlign:"center"};
-
-    const tableStyle = {width:"75%", marginLeft:"auto", marginRight:"auto", borderSpacing:"30px"};
-    const logoStyle = {borderRight:"10px solid lightgrey"};
-
-    const imageStyle = {display:"block", margin:"auto"};
-
-    const cellStyle = {paddingLeft:"24px"};
-
-    const authorStyle = {paddingTop:"24px", textAlign:"right", fontStyle:"italic"};
+    <style dangerouslySetInnerHTML={{ __html: styles }} />
 
     const quotes = [{text:"Some of the best ROI I've achieved for my time is from Plotly and Dash. Plotly is a major thought leader in data science right now.", author:"Dan Kaiminsky, Chief Scientist @ WhiteOps", logoImg:"https://s3-us-west-1.amazonaws.com/plotly-tutorials/quotes-logos/whiteops.png"},
     {text:"Plotly was the big business intelligence killer we'd been waiting for.", author:"Lead Data Scientist @ Goji Auto Insurance", logoImg:"https://s3-us-west-1.amazonaws.com/plotly-tutorials/quotes-logos/goji.png"},
@@ -27,20 +18,21 @@ class Quotes extends React.Component {
     const generateQuotesTable = () => {
         return quotes.map((quote) => {
             return (
-                <table style={{tableStyle}}>
                     <tr>
-                        <td style={{logoStyle}}><img style={{imageStyle}} src={quote.logoImg}/></td>
-                        <td style={{cellStyle}}><div>{quote.text}</div><div style={{authorStyle}}>{quote.author}</div></td>
+                        <td className="logo"><img src={quote.logoImg}/></td>
+                        <td className="cell"><div>{quote.text}</div><div className="author">{quote.author}</div></td>
                     </tr>
-                </table>
           )
         })
       };
 
     return (
         <div>
-            <h1 style={titleStyle}>So, you've heard about Dash...</h1>
-            {generateQuotesTable()}
+            <style dangerouslySetInnerHTML={{ __html: styles }} />
+            <h1 className="title">So, you've heard about Dash...</h1>
+            <table className="quotesTable">
+                {generateQuotesTable()}
+            </table>
         </div>
     )
   }
