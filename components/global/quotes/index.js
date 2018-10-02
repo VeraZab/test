6804e2b0a8e1c685943500d5ad300411045ad945
Joseph Damiba <joseph.damiba@plot.ly>
2018-10-02 00:43:55 -0400

    formatting

--- components/global/quotes/index.js
+++ components/global/quotes/index.js
@@ -3,12 +3,7 @@ import React from 'react'
 class Quotes extends React.Component {
   constructor(props) {
     super(props)
-
-
   }
-
-
-
   render() {
     const titleStyle = {paddingTop:"30px", paddingBottom:"30px", textAlign:"center"};
 
@@ -24,7 +19,6 @@ class Quotes extends React.Component {
     const authorStyle = {paddingTop:"24px", textAlign:"right"};
     return (
         <div>
-
             <h1 style={titleStyle}>So, you've heard about Dash...</h1>
 
                     <table style={tableStyle}>
@@ -34,18 +28,18 @@ class Quotes extends React.Component {
                         </tr>
 
                         <tr>
-                        <td style={cellStyle}><img style={imageStyle} src="https://s3-us-west-1.amazonaws.com/plotly-tutorials/quotes-logos/goji.png"/></td>
-                        <td style={logoStyle}>Plotly was the big business intelligence killer we'd been waiting for. <div style={authorStyle}>&mdash;<i> Lead Data Scientist @ Goji Auto Insurance</i></div></td>
+                            <td style={cellStyle}><img style={imageStyle} src="https://s3-us-west-1.amazonaws.com/plotly-tutorials/quotes-logos/goji.png"/></td>
+                            <td style={logoStyle}>Plotly was the big business intelligence killer we'd been waiting for. <div style={authorStyle}>&mdash;<i> Lead Data Scientist @ Goji Auto Insurance</i></div></td>
                         </tr>
 
                         <tr>
-                        <td style={cellStyle}><img style={imageStyle} src="https://s3-us-west-1.amazonaws.com/plotly-tutorials/quotes-logos/Invesco.png"/></td>
-                        <td style={logoStyle}>Dash has given our quant team a powerful framework for next generation analytical apps.<div style={authorStyle}>&mdash;<i> Julio Guzman, Sr. Manager of Technology Investments @ Invesco</i></div></td>
+                            <td style={cellStyle}><img style={imageStyle} src="https://s3-us-west-1.amazonaws.com/plotly-tutorials/quotes-logos/Invesco.png"/></td>
+                            <td style={logoStyle}>Dash has given our quant team a powerful framework for next generation analytical apps.<div style={authorStyle}>&mdash;<i> Julio Guzman, Sr. Manager of Technology Investments @ Invesco</i></div></td>
                         </tr>
 
                         <tr>
-                        <td style={cellStyle}><img style={imageStyle} src="https://s3-us-west-1.amazonaws.com/plotly-tutorials/quotes-logos/jupyter.png"/></td>
-                        <td style={logoStyle}>Plotly and Dash bring data to life: interactive exploration tied to the source data and models pulls out insight and lets organizations maximize ROI.<div style={authorStyle}>&mdash;<i> Fernando Pérez, co-founder of Project Jupyter and Plotly Advisor</i></div></td>
+                            <td style={cellStyle}><img style={imageStyle} src="https://s3-us-west-1.amazonaws.com/plotly-tutorials/quotes-logos/jupyter.png"/></td>
+                            <td style={logoStyle}>Plotly and Dash bring data to life: interactive exploration tied to the source data and models pulls out insight and lets organizations maximize ROI.<div style={authorStyle}>&mdash;<i> Fernando Pérez, co-founder of Project Jupyter and Plotly Advisor</i></div></td>
                         </tr>
 
                     </table>
