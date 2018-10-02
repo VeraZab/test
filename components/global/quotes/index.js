df097d3537ead49d3344487500467b4288304570
Joseph Damiba <joseph.damiba@plot.ly>
2018-10-02 09:58:49 -0400

    switched cell and logostyle for clarity

--- components/global/quotes/index.js
+++ components/global/quotes/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { LogicGateOrIcon } from 'mdi-react';
 
 class Quotes extends React.Component {
   constructor(props) {
@@ -9,37 +10,36 @@ class Quotes extends React.Component {
 
     const tableStyle = {width:"75%", marginLeft:"auto", marginRight:"auto", borderSpacing:"30px"};
 
-    const cellStyle = {borderRight:"10px solid lightgrey"};
+    const logoStyle = {borderRight:"10px solid lightgrey"};
 
-    const imageStyle = {display:"block",
-    margin:"auto"};
+    const imageStyle = {display:"block", margin:"auto"};
 
-    const logoStyle = {paddingLeft:"24px"};
+    const cellStyle = {paddingLeft:"24px"};
 
-    const authorStyle = {paddingTop:"24px", textAlign:"right"};
+    const authorStyle = {paddingTop:"24px", textAlign:"right", fontStyle:"italic"};
     return (
         <div>
             <h1 style={titleStyle}>So, you've heard about Dash...</h1>
 
                     <table style={tableStyle}>
                         <tr>
-                            <td style={cellStyle}><img style={imageStyle} src="https://s3-us-west-1.amazonaws.com/plotly-tutorials/quotes-logos/whiteops.png"/></td>
-                            <td style={logoStyle}>Some of the best ROI I've achieved for my time is from Plotly and Dash. Plotly is a major thought leader in data science right now. <div style={authorStyle}>&mdash;<i> Dan Kaiminsky, Chief Scientist @ WhiteOps</i> </div></td>
+                            <td style={logoStyle}><img style={imageStyle} src="https://s3-us-west-1.amazonaws.com/plotly-tutorials/quotes-logos/whiteops.png"/></td>
+                            <td style={cellStyle}>Some of the best ROI I've achieved for my time is from Plotly and Dash. Plotly is a major thought leader in data science right now. <div style={authorStyle}>&mdash; Dan Kaiminsky, Chief Scientist @ WhiteOps</div></td>
                         </tr>
 
                         <tr>
-                            <td style={cellStyle}><img style={imageStyle} src="https://s3-us-west-1.amazonaws.com/plotly-tutorials/quotes-logos/goji.png"/></td>
-                            <td style={logoStyle}>Plotly was the big business intelligence killer we'd been waiting for. <div style={authorStyle}>&mdash;<i> Lead Data Scientist @ Goji Auto Insurance</i></div></td>
+                            <td style={logoStyle}><img style={imageStyle} src="https://s3-us-west-1.amazonaws.com/plotly-tutorials/quotes-logos/goji.png"/></td>
+                            <td style={cellStyle}>Plotly was the big business intelligence killer we'd been waiting for. <div style={authorStyle}>&mdash;Lead Data Scientist @ Goji Auto Insurance</div></td>
                         </tr>
 
                         <tr>
-                            <td style={cellStyle}><img style={imageStyle} src="https://s3-us-west-1.amazonaws.com/plotly-tutorials/quotes-logos/Invesco.png"/></td>
-                            <td style={logoStyle}>Dash has given our quant team a powerful framework for next generation analytical apps.<div style={authorStyle}>&mdash;<i> Julio Guzman, Sr. Manager of Technology Investments @ Invesco</i></div></td>
+                            <td style={logoStyle}><img style={imageStyle} src="https://s3-us-west-1.amazonaws.com/plotly-tutorials/quotes-logos/Invesco.png"/></td>
+                            <td style={cellStyle}>Dash has given our quant team a powerful framework for next generation analytical apps.<div style={authorStyle}>&mdash;Julio Guzman, Sr. Manager of Technology Investments @ Invesco</div></td>
                         </tr>
 
                         <tr>
-                            <td style={cellStyle}><img style={imageStyle} src="https://s3-us-west-1.amazonaws.com/plotly-tutorials/quotes-logos/jupyter.png"/></td>
-                            <td style={logoStyle}>Plotly and Dash bring data to life: interactive exploration tied to the source data and models pulls out insight and lets organizations maximize ROI.<div style={authorStyle}>&mdash;<i> Fernando Pérez, co-founder of Project Jupyter and Plotly Advisor</i></div></td>
+                            <td style={logoStyle}><img style={imageStyle} src="https://s3-us-west-1.amazonaws.com/plotly-tutorials/quotes-logos/jupyter.png"/></td>
+                            <td style={cellStyle}>Plotly and Dash bring data to life: interactive exploration tied to the source data and models pulls out insight and lets organizations maximize ROI.<div style={authorStyle}>&mdash; Fernando Pérez, co-founder of Project Jupyter and Plotly Advisor</div></td>
                         </tr>
 
                     </table>
