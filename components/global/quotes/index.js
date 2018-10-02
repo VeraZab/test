58d68334b92178ef75d8bf698c01a3939b8f62e2
Joseph Damiba <joseph.damiba@plot.ly>
2018-10-02 00:09:29 -0400

    used css to position author quote in bottom right of div

--- components/global/quotes/index.js
+++ components/global/quotes/index.js
@@ -8,18 +8,14 @@ class Quotes extends React.Component {
   render() {
     return (
         <div>
-            <br/><br/><br/>
 
-            <h1 style={{textAlign:"center"}}>So, you've heard about Dash...</h1>
+            <h1 style={{paddingTop:"30px", paddingBottom:"30px", textAlign:"center"}}>So, you've heard about Dash...</h1>
 
-            <br/><br/><br/>
-            <div>
-                <div>
                     <table style={{marginLeft:"auto", marginRight:"auto"}}>
                         <tr>
                             <td style={{borderRight:"10px solid lightgrey"}}><img style={{display:"block",
                         margin:"auto"}} src="https://s3-us-west-1.amazonaws.com/plotly-tutorials/quotes-logos/whiteops.png"/></td>
-                            <td style={{paddingLeft:"24px"}}>Some of the best ROI i've achieved for my time is from Plotly and Dash. Plotly is a <br/>major thought leader in data science right now. <br/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;   &mdash;<i> Dan Kaiminsky, Chief Scientist @ WhiteOps</i> </td>
+                            <td style={{paddingLeft:"24px"}}>Some of the best ROI i've achieved for my time is from Plotly and Dash. Plotly is a <br/>major thought leader in data science right now. <br/> <div style={{textAlign:"right"}}>&mdash;<i> Dan Kaiminsky, Chief Scientist @ WhiteOps</i> </div></td>
                         </tr>
 
                         <br/>
@@ -28,7 +24,7 @@ class Quotes extends React.Component {
                         <tr>
                             <td style={{borderRight:"10px solid lightgrey"}}><img style={{display:"block",
                         margin:"auto"}} src="https://s3-us-west-1.amazonaws.com/plotly-tutorials/quotes-logos/goji.png"/></td>
-                            <td style={{paddingLeft:"24px"}}>Plotly was the big business intelligence killer we'd been waiting for.<br/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&mdash;<i> Lead Data Scientist @ Goji Auto Insurance</i><br/><br/></td>
+                            <td style={{paddingLeft:"24px"}}>Plotly was the big business intelligence killer we'd been waiting for.<br/> <div style={{textAlign:"right"}}>&mdash;<i> Lead Data Scientist @ Goji Auto Insurance</i></div><br/><br/></td>
                         </tr>
 
                         <br/>
@@ -37,7 +33,7 @@ class Quotes extends React.Component {
                         <tr>
                             <td style={{borderRight:"10px solid lightgrey"}}><img style={{display:"block",
                         margin:"auto"}} src="https://s3-us-west-1.amazonaws.com/plotly-tutorials/quotes-logos/Invesco.png"/></td>
-                            <td style={{paddingLeft:"24px"}}>Dash has given our quant team a powerful framework for next generation<br/> analytical apps.<br/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&mdash;<i> Julio Guzman, Sr. Manager of Technology Investments @ Invesco</i></td>
+                            <td style={{paddingLeft:"24px"}}>Dash has given our quant team a powerful framework for next generation<br/> analytical apps.<br/> <div style={{textAlign:"right"}}>&mdash;<i> Julio Guzman, Sr. Manager of Technology Investments @ Invesco</i></div></td>
                         </tr>
 
                         <br/>
@@ -46,14 +42,12 @@ class Quotes extends React.Component {
                         <tr>
                             <td style={{borderRight:"10px solid lightgrey"}}><img style={{display:"block",
                         margin:"auto"}} src="https://s3-us-west-1.amazonaws.com/plotly-tutorials/quotes-logos/jupyter.png"/></td>
-                            <td style={{paddingLeft:"24px"}}>Plotly and Dash bring data to life: interactive exploration tied to the source data and <br/> models pulls out insight and lets organizations maximize ROI. <br/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&mdash;<i> Fernando Pérez, co-founder of Project Jupyter and Plotly Advisor</i></td>
+                            <td style={{paddingLeft:"24px"}}>Plotly and Dash bring data to life: interactive exploration tied to the source data and <br/> models pulls out insight and lets organizations maximize ROI. <br/> <div style={{textAlign:"right"}}>&mdash;<i> Fernando Pérez, co-founder of Project Jupyter and Plotly Advisor</i></div></td>
                         </tr>
 
                         <br/>
                         <br/>
                     </table>
-                    </div>
-                </div>
             </div>
     )
   }
