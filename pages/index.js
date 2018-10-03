585444f37ed0c4cd272ce058666682ef8f493234
Joseph Damiba <joseph.damiba@plot.ly>
2018-10-03 00:07:12 -0400

    formatting

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import React from 'react'
 
-
 class Index extends React.Component {
   constructor(props) {
     super(props)
   }
 
-
   render() {
     return <div />
   }
 }
+
 export default Index
