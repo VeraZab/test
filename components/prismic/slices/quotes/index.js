f45d303c442460109fdbe8cb41705fec8d791586
Joseph Damiba <joseph.damiba@plot.ly>
2018-10-04 13:20:47 -0400

    changed name of scss classes to flexContainer and flexItem

--- components/prismic/slices/quotes/index.js
+++ components/prismic/slices/quotes/index.js
@@ -13,12 +13,11 @@ class Quotes extends React.Component {
             <style dangerouslySetInnerHTML={{ __html: styles }} />
             <h1 className="title">{this.props.data.primary.title1[0].text}</h1>
 
-            <div className="quotesContainer">
-
+            <div className="flexContainer">
                     {
                         this.props.data.items.map((prop) => {
                             return (
-                                <div className="quoteItem">
+                                <div className="flexItem">
                                     <div className="logo"><img className="logoImage" alt="client logo" src={prop.logo.url}/></div>
                                     <div className="cell"><div>{prop.quote1[0].text}</div><div className="author">{prop.author1[0].text}</div></div>
                                 </div>
