02e81d1fe32ed267dccab616be1b139db4f2b5b2
Joseph Damiba <joseph.damiba@plot.ly>
2018-10-30 23:28:39 -0400

    leading the head component unchanged, adding tag to _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -26,6 +26,13 @@ export default class MyDocument extends Document {
             gtag('js', new Date());
             gtag('config', 'AW-1009791370');`}}
           />
+          <script dangerouslySetInnerHTML={{__html:
+            `(
+              function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-N6T2RXG');`}} />
           <script dangerouslySetInnerHTML={
             {__html: `window.prismic = {endpoint: 'https://plotly.prismic.io/api/v2'};`}}
           />
