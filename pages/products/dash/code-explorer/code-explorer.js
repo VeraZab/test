439f33b826c92219d527c71eda1abaf6d491ffe2
Joseph Damiba <joseph.damiba@plot.ly>
2019-01-17 12:53:31 -0500

    updating dash splash example

--- pages/products/dash/code-explorer/code-explorer.js
+++ pages/products/dash/code-explorer/code-explorer.js
@@ -3,42 +3,119 @@ import SyntaxHighlighter from 'react-syntax-highlighter';
 import {atomOneDark} from 'react-syntax-highlighter/dist/styles';
 
 const jsCode = `import dash
-from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
-from pandas_datareader import data as web
-from datetime import datetime as dt
 
-app = dash.Dash()
+import colorlover as cl
+import datetime as dt
+import flask
+import os
+import pandas as pd
+import time
+
+app = dash.Dash(
+    __name__,
+    assets_external_scripts='https://cdn.plot.ly/plotly-finance-1.28.0.min.js'
+)
+server = app.server
+
+app.scripts.config.serve_locally = False
+
+colorscale = cl.scales['9']['qual']['Paired']
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/dash-stock-ticker-demo.csv')
 
 app.layout = html.Div([
-    html.H1('Stock Tickers'),
+    html.Div([
+        html.H2('Finance Explorer',
+                style={'display': 'inline',
+                       'float': 'left',
+                       'font-size': '2.65em',
+                       'margin-left': '7px',
+                       'font-weight': 'bolder',
+                       'font-family': 'Product Sans',
+                       'color': "rgba(117, 117, 117, 0.95)",
+                       'margin-top': '20px',
+                       'margin-bottom': '0'
+                       }),
+        html.Img(src="https://s3-us-west-1.amazonaws.com/plotly-tutorials/logo/new-branding/dash-logo-by-plotly-stripe.png",
+                style={
+                    'height': '100px',
+                    'float': 'right'
+                },
+        ),
+    ]),
     dcc.Dropdown(
-        id='my-dropdown',
-        options=[
-            {'label': 'Coke', 'value': 'COKE'},
-            {'label': 'Tesla', 'value': 'TSLA'},
-            {'label': 'Apple', 'value': 'AAPL'}
-        ],
-        value='COKE'
+        id='stock-ticker-input',
+        options=[{'label': s[0], 'value': str(s[1])}
+                 for s in zip(df.Stock.unique(), df.Stock.unique())],
+        value=['YHOO', 'GOOGL'],
+        multi=True
     ),
-    dcc.Graph(id='my-graph')
-])
+    html.Div(id='graphs')
+], className="container")
 
-@app.callback(Output('my-graph', 'figure'), [Input('my-dropdown', 'value')])
-def update_graph(selected_dropdown_value):
-    df = web.DataReader(
-        selected_dropdown_value, data_source='google',
-        start=dt(2017, 1, 1), end=dt.now())
-    return {
-        'data': [{
-            'x': df.index,
-            'y': df.Close
-        }]
-    }
+def bbands(price, window_size=10, num_of_std=5):
+    rolling_mean = price.rolling(window=window_size).mean()
+    rolling_std  = price.rolling(window=window_size).std()
+    upper_band = rolling_mean + (rolling_std*num_of_std)
+    lower_band = rolling_mean - (rolling_std*num_of_std)
+    return rolling_mean, upper_band, lower_band
+
+@app.callback(
+    dash.dependencies.Output('graphs','children'),
+    [dash.dependencies.Input('stock-ticker-input', 'value')])
+
+def update_graph(tickers):
+    graphs = []
+
+    if not tickers:
+        graphs.append(html.H3(
+            "Select a stock ticker.",
+            style={'marginTop': 20, 'marginBottom': 20}
+        ))
+    else:
+        for i, ticker in enumerate(tickers):
+
+            dff = df[df['Stock'] == ticker]
+
+            candlestick = {
+                'x': dff['Date'],
+                'open': dff['Open'],
+                'high': dff['High'],
+                'low': dff['Low'],
+                'close': dff['Close'],
+                'type': 'candlestick',
+                'name': ticker,
+                'legendgroup': ticker,
+                'increasing': {'line': {'color': colorscale[0]}},
+                'decreasing': {'line': {'color': colorscale[1]}}
+            }
+            bb_bands = bbands(dff.Close)
+            bollinger_traces = [{
+                'x': dff['Date'], 'y': y,
+                'type': 'scatter', 'mode': 'lines',
+                'line': {'width': 1, 'color': colorscale[(i*2) % len(colorscale)]},
+                'hoverinfo': 'none',
+                'legendgroup': ticker,
+                'showlegend': True if i == 0 else False,
+                'name': '{} - bollinger bands'.format(ticker)
+            } for i, y in enumerate(bb_bands)]
+            graphs.append(dcc.Graph(
+                id=ticker,
+                figure={
+                    'data': [candlestick] + bollinger_traces,
+                    'layout': {
+                        'margin': {'b': 0, 'r': 10, 'l': 60, 't': 0},
+                        'legend': {'x': 0}
+                    }
+                }
+            ))
+
+    return graphs
 
 if __name__ == '__main__':
-    app.run_server()`;
+    app.run_server(debug=True)`;
 
 const languages = [
   {
@@ -103,7 +180,7 @@ export default class CodeExplorer extends React.Component {
         <div className="code-explorer-wrapper">
           <div className="code-explorer-graphic">
             <div className="code-explorer-graphic-wrapper desktop-graphic">
-              <iframe src="https://dash-hello-world.herokuapp.com/" />
+              <iframe src="https://dash-gallery.plotly.host/dash-stock-tickers-demo-app" />
             </div>
             <div className="code-explorer-graphic-wrapper mobile-graphic">
               <div className="graphic-img-wrapper">
