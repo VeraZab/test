984612dfa3250cbba909293a1f3c0ab2129d0529
Joseph Damiba <joseph.damiba@plot.ly>
2019-02-05 15:11:57 -0500

    styling fixups

--- components/prismic/slices/newsroom.js
+++ components/prismic/slices/newsroom.js
@@ -15,25 +15,29 @@ class Newsroom extends React.Component {
             <Body>
               {this.props.data.items.map((item, i) => {
                 return (
-                  <div className="webinar-wrapper">
-                    <div className="webinar-section" key={i}>
-                      <div className="webinar-img">
-                        {item.story_image ? (
-                          <div
-                            className="webinar-img-wrapper"
-                            style={{
-                              backgroundImage:
-                                'url(' + item.story_image.url + '?w=100&h=100&fit=crop)',
-                              backgroundSize: 'cover',
-                              backgroundPosition: 'center center',
-                            }}
-                          />
-                        ) : (
-                          ''
-                        )}
-                      </div>
+                  <div className="webinar-wrapper" key={i}>
+                    <div className="webinar-section">
+                      <a href={item.story_url[0].text}>
+                        <div className="webinar-img">
+                          {item.story_image ? (
+                            <div
+                              className="webinar-img-wrapper"
+                              style={{
+                                backgroundImage:
+                                  'url(' + item.story_image.url + '?w=100&h=100&fit=crop)',
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center center',
+                              }}
+                            />
+                          ) : (
+                            ''
+                          )}
+                        </div>
+                      </a>
                       <div className="webinar-details">
-                        <div className="webinar-details-title">{item.story_title[0].text}</div>
+                        <a href={item.story_url[0].text}>
+                          <div className="webinar-details-title">{item.story_title[0].text}</div>
+                        </a>
                         <div className="webinar-details-row">
                           <div className="webinar-details-date">
                             <span>When:</span>
@@ -44,20 +48,6 @@ class Newsroom extends React.Component {
                             {item.story_presenter[0].text}
                           </div>
                         </div>
-                      </div>
-                    </div>
-                    <div className="webinar-section">
-                      <div className="webinar-actions">
-                        <Button
-                          item={{
-                            label: 'Read More',
-                            subtitle: null,
-                            title: 'Read More',
-                            icon: null,
-                            button: {classes: 'button button-primary'},
-                            link: item.story_url[0].text,
-                          }}
-                        />
                       </div>
                     </div>
                   </div>
