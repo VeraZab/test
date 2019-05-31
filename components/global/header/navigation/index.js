05f80cec66638d6c0bce22cecb376651a2523dac
Joseph Damiba <joseph.damiba@plot.ly>
2019-05-31 13:04:49 -0400

    fixup

--- components/global/header/navigation/index.js
+++ components/global/header/navigation/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-
-const DEMO_FORM_URL = 'https://go.plot.ly/l/719653/2019-05-30/2f6sb';
+const DEMO_FORM_URL = 'https://plotly.typeform.com/to/ECUfbT';
 const MENU = {
   PRODUCTS: {
     LEFT: [
@@ -312,7 +311,7 @@ export default class Navigation extends React.Component {
             <div className="cta-buttons">
               <a
                 id="header-button"
-                href={MENU.CTA.link}
+                href="https://go.plot.ly/l/719653/2019-05-30/2f6sb"
                 className="button button-primary header-button"
               >
                 <div className="button-label">{MENU.CTA.title}</div>
