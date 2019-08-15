82189557bced2e68c73621b00ffdbc725f0fd1f5
Vera Zabeida <vera.zabeida@gmail.com>
2019-08-15 18:04:11 -0400

    removing mailchimp start

--- components/global/mailchimp-form/index.js
+++ components/global/mailchimp-form/index.js
@@ -3,51 +3,108 @@ class MailChimpForm extends React.Component {
     super(props);
     this.state = {
       emailValue: '',
+      formSuccess: false,
+      formError: null,
     };
   }
 
   render() {
     return (
       <div>
+        <iframe
+          name="hiddenFrame"
+          style={{position: 'absolute', top: '-1px', left: '-1px', width: '1px', height: '1px'}}
+          onLoad={() => this.setState({formSuccess: true})}
+          onError={err => this.setState({formError: err})}
+        ></iframe>
+
         <form
           className="footer-main-section"
-          action="https://plot.us12.list-manage.com/subscribe/post"
+          action="https://go.plot.ly/subscribe"
           method="POST"
-          target="_blank"
           noValidate
+          target="hiddenFrame"
         >
-          <input type="hidden" name="u" value="28d7f8f0685d044fb51f0d4ee" />
-          <input type="hidden" name="id" value="0c1cb734d7" />
+          <input
+            style={{display: 'none'}}
+            type="checkbox"
+            name="719653_26097pi_719653_26097_326347"
+            id="719653_26097pi_719653_26097_326347"
+            value="326347"
+            checked
+          />
+          <input
+            style={{display: 'none'}}
+            type="checkbox"
+            name="719653_26097pi_719653_26097_326349"
+            id="719653_26097pi_719653_26097_326349"
+            value="326349"
+            checked
+          />
+          <input
+            style={{display: 'none'}}
+            type="checkbox"
+            name="719653_26097pi_719653_26097_326351"
+            id="719653_26097pi_719653_26097_326351"
+            value="326351"
+            checked
+          />
+          <input
+            style={{display: 'none'}}
+            type="checkbox"
+            name="719653_26097pi_719653_26097_326353"
+            id="719653_26097pi_719653_26097_326353"
+            value="326353"
+            checked
+          />
 
           <label htmlFor="MERGE0">
             <div className="footer-main-section-header">Join the Dash Club</div>
-            <p className="mailchimp-footer-text">
-              {' '}
-              Dash Club is a no-fluff, twice-a-month email with links and notes on the latest Dash
-              developments and community happenings.
-            </p>
-            <input
-              type="email"
-              name="EMAIL"
-              id="MERGE0"
-              placeholder="Your Email Address"
-              value={this.state.emailValue}
-              onChange={e => {
-                this.setState({emailValue: e.target.value});
-              }}
-              autoCapitalize="off"
-              autoCorrect="off"
-              className="mc-input"
-            />
+            {this.state.formSuccess ? (
+              <div style={{color: 'white'}}>
+                <div> Thank you for subscribing! </div>
+                <div>
+                  <a href="https://go.plot.ly/subscribe" target="_blank">
+                    You can change your email preferences here.
+                  </a>
+                </div>
+              </div>
+            ) : this.state.formError ? (
+              <div>{`Sorry, an unexpected error occured: ${this.state.formError}`}</div>
+            ) : (
+              <>
+                <p className="mailchimp-footer-text">
+                  {' '}
+                  Dash Club is a no-fluff, twice-a-month email with links and notes on the latest
+                  Dash developments and community happenings.
+                </p>
+                <input
+                  type="text"
+                  name="Emailpi_Email"
+                  id="Emailpi_Email"
+                  placeholder="Your Email Address"
+                  value={this.state.emailValue}
+                  onChange={e => {
+                    this.setState({emailValue: e.target.value});
+                  }}
+                  autoCapitalize="off"
+                  autoCorrect="off"
+                  className="mc-input"
+                />
+              </>
+            )}
           </label>
-          <input
-            type="submit"
-            value="Subscribe"
-            name="subscribe"
-            id="mc-embedded-subscribe"
-            className="button button-primary"
-          />
+          {this.state.formSuccess ? null : (
+            <input
+              type="submit"
+              value="Subscribe"
+              name="subscribe"
+              id="mc-embedded-subscribe"
+              className="button button-primary"
+            />
+          )}
         </form>
+
         <div className="social-icons-wrapper">
           <div className="social-icons">
             <a href="https://github.com/plotly">
